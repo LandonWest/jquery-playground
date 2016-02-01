cbf5156f48d6df1d8ba67b0a4f0286e1b688e416
LandonWest <landonwest5@gmail.com>
2016-01-31 22:15:09 -0800

    circles fade in and display inline on document ready

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Main javaScript
+$(document).ready(function(){
+  $('#green_circle').fadeIn(3000);
+  $('#orange_circle').fadeIn(6000);
+  $('#blue_circle').fadeIn(10000);
+  $('div').css('display', 'inline-block');
+});
