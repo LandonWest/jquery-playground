afd305d833ec245c07bdeea72a33f55ea8b8c406
LandonWest <landonwest5@gmail.com>
2016-02-01 21:40:52 -0800

    refactor html/css; add styles

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Main javaScript
 $(document).ready(function(){
-  $('#green_circle').fadeIn(3000);
-  $('#orange_circle').fadeIn(6000);
-  $('#blue_circle').fadeIn(10000);
-  $('div').css('display', 'inline-block');
+  $('#green_circ').fadeIn(3000);
+  $('#orange_circ').fadeIn(6000);
+  $('#blue_circ').fadeIn(10000);
+  $('div.circle').css('display', 'inline-block');
 });
