99500f7fffed2ab6c03684c8786f2de8cba4f3d7
LandonWest <landonwest5@gmail.com>
2016-02-02 22:05:15 -0800

    add vanishing ninja section and styles

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ $(document).ready(function(){
   $('#orange_circ').fadeIn(6000);
   $('#blue_circ').fadeIn(10000);
   $('div.circle').css('display', 'inline-block');
+
+  $('img#ninja').mouseenter(function(){
+    $(this).fadeTo(0, 0);
+  });
+  $('img#ninja').mouseleave(function(){
+    $(this).fadeTo(1000, 1);
+  });
 });
