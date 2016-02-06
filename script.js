cd1f5a45f1f7e8337aa3c23a2aad95673dcf99e3
LandonWest <landonwest5@gmail.com>
2016-02-05 20:50:15 -0800

    toggle switch on/off; add dark room & eyes

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // Main javaScript
+
+// Disappearing circles
 $(document).ready(function(){
   $('#green_circ').fadeIn(3000);
   $('#orange_circ').fadeIn(6000);
   $('#blue_circ').fadeIn(10000);
   $('div.circle').css('display', 'inline-block');
-
+// Ninja disappear
   $('img#ninja').mouseenter(function(){
     $(this).fadeTo(0, 0);
   });
   $('img#ninja').mouseleave(function(){
     $(this).fadeTo(1000, 1);
+  });
+  //  Light switch
+  $('div.switch p').on('click', function(){
+    $(this).html() == 'ON' ? $(this).html('OFF') : $(this).html('ON');
   });
 });
